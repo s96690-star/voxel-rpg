4e481bd9115ee1ceee480ec77cbe9ecf03177af1
s96690-star <s96690@sfps.k12.nm.us>
2025-12-14 15:21:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+import { Game } from "./src/engine/GameLoop.js";
 
+const canvas = document.getElementById("game");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const game = new Game(canvas);
+game.start();
